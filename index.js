a7315e61292706b7ac90fd31993a990e56dc9733
FarsanBaloo <106886894+FarsanBaloo@users.noreply.github.com>
2023-10-18 01:43:54 +0200

    ..

--- index.js
+++ index.js
@@ -1,111 +1,116 @@
-
 import fs from 'fs';
 import Personer from "./persons.js";
-
 import promptSync from 'prompt-sync';
+
 const prompt = promptSync({ sigint: true });
 
-// När vi skapar ett nytt Hundar-objekt kommer constructor att
-// läsa in hundarna från JSON-filen. 
+// When we create a new Personer object, the constructor will
+// read in the persons from the JSON file.
 const personLista = new Personer();
 
+function remove() {
+  personLista.skrivUtPersoner();
+  const val = prompt("Enter the index of the one you want to remove ->");
 
-
-function removePerson() {
-  personLista.skrivUtPersoner(); // Skriver ut listan på alla hundar med index i början.
-  const val = prompt("Skriv in index för det du vill tabort ->");
-
-  if (Number(val).toString() === "NaN") { // Kollar så att val går att parsa till ett nummer.
-    console.log("Måste skriva in ett tal!");
+  if (isNaN(Number(val))) { 
+    console.log("You must enter a number!");
+    return;
   }
   if (val <= personLista.getLength() && val >= 1) {
-    hundLista.removePersonFromList(Number(val) - 1); // Tar det inskrivna valet och minskar med 1. (för arrays index börjar på 0)
+    personLista.removePersonFromList(Number(val) - 1); 
   } else {
-    console.log(`Talet måste vara mellan 1 och ${personLista.getLength()}`);
+    console.log(`The number must be between 1 and ${personLista.getLength()}`);
   }
 }
 
-
 while (true) {
-  console.clear();
-
-  console.log('Meny Musik databasen');
-  console.log(''); // Empty line
-
+  console.clear()
+  console.log(''); 
+  console.log('Meny Musik och band databasen');
+  console.log(''); 
   console.log('1. Skapa ett band');
   console.log('2. Ta bort ett band');
   console.log('3. Skapa en musiker');
   console.log('4. Ta bort en musiker');
-  console.log('5. Lägg till en musiker i ett band');
+  console.log('5. Lägg till en musiker till ett band');
   console.log('6. Ta bort en musiker från ett band');
-  console.log('7. Skriv ut musiker och band');
+  console.log('7. Skriv ut inlaggda musiker och band');
   console.log('8. Avsluta programmet');
+  console.log(''); 
 
-  console.log(''); // Empty line
-  let val = prompt('Välj ett alternativ: ');
-
+  let val = prompt('Välj ett meny vall: ');
 
   switch (val) {
-  
-    case "1": // Lägg till ett band i databasen
+
+
+    case "1": // Skapa ett band
       const nameofband = prompt('Ange namnet på bandet: ');
-      const yearstarted = Number(prompt('Ange årtalet bandet skapades: '));
-
-      personLista.addBandToList(nameofband, yearstarted);
-
+      const yearstarted = Number(prompt('Ange årtalet som bandet skappades: '));
+      if (!isNaN(yearstarted)) {
+        personLista.addBandToList(nameofband, yearstarted);
+      } else {
+        console.log("Ogiltigt inmatning av år ange ett tal.");
+      }
       break;
 
-    
-    case "2":   // Ta bort ett band i databasen
-    removePerson();
-    break;
-
-    
-    case 3: // Skapa en musiker i databasen
-
-    const nameofartist = prompt('Ange namnet på musikern: ');
-    const yearbirth = Number(prompt('Ange årtalet när artisten är född [19xx]: '));
-
-    personLista.addMusikerToList(nameofartist, yearbirth);
-   
-    break;
-
-    case "3":   // Ta bort en musiker ifrån databasen
-    // Remove a musician from JSON
-    const artistNameToRemove = prompt('Ange namnet på musikern som ska tas bort från databasen: ');
-    // Remove the musician by name
-    removePerson();
-    break;
-
-    case "5": // Lägg till musiker i ett band
-
-    
-      personLista.skrivUtPersoner();
-    
- 
+    case "2": // Ta bort ett band
+      remove();
       break;
+
+    case "3": // Skapa en musiker
+      const nameofartist = prompt('Ange namnet på musikanten: ');
+      const yearbirth = Number(prompt('Ange födelse året: '));
+      if (!isNaN(yearbirth)) {
+        personLista.addMusikerToList(nameofartist, yearbirth);
+      } else {
+        console.log("Ogiltigt inmatning av år ange ett tal.");
+      }
+      break;
+
+    case "4": // Ta bort en musiker
+      remove();
+      break;
+
+      case "5": // Lägg till en musiker till ett band
+      personLista.skrivUtPersoner(); 
+      const musikerIndex = Number(prompt('Ange numret för musikanten i listan som skall läggas till ett band: ')) - 1; // we subtract 1 because array indexes start from 0
+      const bandIndex = Number(prompt('Ange numret som bandet har i listan: ')) - 1; 
+      if (!Number.isNaN(musikerIndex) && !Number.isNaN(bandIndex) && 
+          musikerIndex >= 0 && bandIndex >= 0 && 
+          musikerIndex < personLista.getLength() && bandIndex < personLista.getLength()) {
+        personLista.addMusikerToBand(musikerIndex, bandIndex);
+      } else {
+        console.log('Ogiltigt nummer utför listans giltiga område ');
+      }
+      prompt('Tryck enter för att återgå till menyn')
+      break;
+
+
+
+    case "6": // Ta bort en musiker ifrån ett band
+      personLista.skrivUtPersoner(); //
+      const musikerIndex1 = Number(prompt('Ange numret för musikanten i listan som skall tas bort från ett band: ')) - 1; // we subtract 1 because array indexes start from 0
+      const bandIndex1 = Number(prompt('Ange numret som bandet har i listan: ')) - 1;
+      if (!Number.isNaN(musikerIndex1) && !Number.isNaN(bandIndex1) && 
+          musikerIndex >= 0 && bandIndex >= 0 && 
+          musikerIndex < personLista.getLength() && bandIndex < personLista.getLength()) {
+        personLista.removeMusikerFromBand(musikerIndex, bandIndex);
+      } else {
+        console.log('Ogiltigt nummer utför listans giltiga område ');
+      }
     
+      break;
 
-  case 6: // Tabort en musiker ifrån ett band
-      removePerson();
-    break;
-    
+    case "7": // Skriv ut inlaggda musiker och band
+      personLista.skrivUtPersoner(); // 
+      prompt('Tryck enter för att återgå till menyn')
+      break;
 
-  case "7": // Avsluta programet
-    process.exit(0);
-    break;
+    case "8": // Avsluta programmet
+      process.exit(0);
 
-  case "8": // Skriv ut alla musiker och band
-  personLista.skrivUtPersoner(); // Skriver ut listan på alla hundar med index i början.
-
-
-  default:
-    console.log('Ogiltigt val, försök igen!');
-    break;  
-          
-  }   
-    
-
-
+    default:
+      console.log('Ogiltigt meny vall försök igen!');
+      break;
+  }
 }
-
--- persons.js
+++ persons.js
@@ -1,95 +1,103 @@
 import fs from "fs";
-import {Band, Musiker} from "./person.js";
+import { Band } from "./Band.js";
+import { Musiker } from "./Musiker.js";
 
-
-export default class Personer
-{
-  #persons = []; // Lista som håller alla Person-objekt.
+export default class Personer {
+  #persons = []; // Listan som håller alla musiker och band
 
   constructor() {
-    this.#fetchPersonData();
+    this.#fetchData();
   }
 
   get persons() {
     return this.#persons;
   }
 
-
-  // Läser in alla database från "database.json". 
-  #fetchPersonData() 
-  {
+  // Reads all database from "database.json".
+  #fetchData() {
     const jsonString = fs.readFileSync("database.json");
     const data = JSON.parse(jsonString);
-    
 
-    // Populerar #persons med Band-objekt, då kommer vi få tillgång till alla metoder i Band-klassen.
-    if (this instanceof Band) 
-    {
-      for (let i = 0; i < data.length; i++) {
-        this.#persons.push(new Band(data[i].name,data[i].info, data[i].yearstarted, data[i].yearended, data[i].current, data[i].earlier));
-      }
-    } 
-      
-         // Populerar #persons med person-objekt, då kommer vi få tillgång till alla metoder i Musiker-klassen.
-    if (this instanceof Musiker) 
-    {
-      for (let i = 0; i < data.length; i++) {
-        this.#persons.push(new Musiker(data[i].name,data[i].info, data[i].yearbirth, data[i].instrumentations, data[i].current, data[i].earlier));
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].Info === "Band") {
+        this.#persons.push(new Band(data[i].Name, data[i].Yearstarted, data[i].Yearended));
+      } else if (data[i].Info === "Musiker") {
+        this.#persons.push(new Musiker(data[i].Name, data[i].Yearbirth));
       }
     }
-  } 
-  
+  }
 
-  //Skriver ut index och Person-objektens namn 
-  skrivUtPersoner() 
-  {
+  skrivUtPersoner() {
     for (let i = 0; i < this.#persons.length; i++) {
       console.log(`${i + 1}. ${this.#persons[i].name}`);
     }
   }
 
-  addMusikerToList(namemusiker, yearbirth) 
-  {
-    this.#persons.push(new Musiker(namemusiker,yearbirth)); // Lägger till en ny Musiker i #persons.
-    this.#updateJsonFile(); // Uppdaterar "database.json".
+  addMusikerToList(namemusiker, yearbirth) {
+    this.#persons.push(new Musiker(namemusiker, yearbirth)); 
+    this.#updateJsonFile(); 
   }
 
-  addBandToList(nameband, yearstarted) 
-  {
-    this.#persons.push(new Band(nameband,yearstarted)); // Lägger till en ny Band i #persons.
-    this.#updateJsonFile(); // Uppdaterar "database.json".
+  addBandToList(nameband, yearstarted) {
+    this.#persons.push(new Band(nameband, yearstarted)); 
+    this.#updateJsonFile(); 
   }
+
+  addMusikerToBand(musikerIndex, bandIndex) {
+
+    const band = this.#persons[bandIndex];
+    const musiker = this.#persons[musikerIndex];
   
-  
-  removePersonFromList(index) 
-  {
-    this.#persons.splice(index, 1); // Tar bort en Person ifrån #persons.
-    this.#updateJsonFile(); // Uppdaterar "database.json".
-  }
-
-    #updateJsonFile() 
-    {
-      let tempList = []; // Skapar en temporär lista som ska sparas i "database.json".
-
-      for (let i = 0; i < this.#persons.length; i++) {
-        // Använder dataInfo som ger mig ett nytt objekt med alla Person-objektet egenskaps information.
-        // Om vi sparar Person-objektet direkt, kommer inte informationen från privata egenskaper med.
-        tempList.push(this.#persons[i].databandInfo());
+    if (band instanceof Band && musiker instanceof Musiker) {
+      // Check if the musician is already in the band
+      const alreadyInBand = band.current.some(memberName => memberName === musiker.name);
+      if (alreadyInBand) {
+        console.log(`${musiker.name} är redan medlem i ett band ${band.name}.`);
+      } else {
+        band.current.push(musiker.name); // Add the musician to the band's list of members
+        musiker.current.push(band.name); // Reflect that the musician is part of this band
+        console.log(`${musiker.name} har laggt till i ${band.name}.`);
+        this.#updateJsonFile(); // Update the JSON file with the new data
       }
-
-      fs.writeFileSync('./database.json', JSON.stringify(tempList, null, 2), (err) => {
-        if (err) throw err;
-        console.log('Data written to file');
-      });
-    } 
-
-
-    getLength() 
-    {
-      return this.#persons.length;
+    } else {
+      console.log('Ogiltigt vall');
     }
   
-  
-} 
- 
-  
+
+    // Add the musician to the band's current members
+    if (!band.current.includes(musiker.name)) {
+      band.current.push(musiker.name);
+    }
+
+    // Add the band to the musician's current band
+    if (!musiker.current.includes(band.name)) {
+      musiker.current.push(band.name);
+    }
+
+    this.#updateJsonFile();
+  }
+
+  removePersonFromList(index) {
+    this.#persons.splice(index, 1); 
+    this.#updateJsonFile(); // Updates "database.json".
+  }
+
+  getLength() {
+    return this.#persons.length;
+  }
+
+  #updateJsonFile() {
+    let tempList = [];
+
+    for (let i = 0; i < this.#persons.length; i++) {
+      if (this.#persons[i] instanceof Band) {
+        tempList.push(this.#persons[i].databandInfo());
+      } else if (this.#persons[i] instanceof Musiker) {
+        tempList.push(this.#persons[i].datamusikerInfo());
+      }
+    }
+
+    fs.writeFileSync('./database.json', JSON.stringify(tempList, null, 2));
+    console.log('Data written to file');
+  }
+}
